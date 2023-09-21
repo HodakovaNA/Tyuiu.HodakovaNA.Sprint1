884aaafc00f436308bff61be2e4305e12e1262f3
HodakovaNA <144614378+HodakovaNA@users.noreply.github.com>
2023-09-21 23:00:02 +0500

    Add files via upload

---
+++ Tyuiu.HodakovaNA.Sprint1.Task2.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.HodakovaNA.Sprint1.Task2.V13.Lib;
+
+namespace Tyuiu.HodakovaNA.Sprint1.Task2.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2; //Миля 
+            //Миля в км
+            var res = ds.ConvertMilesToKm(x);
+            Assert.AreEqual(3218, res);
+        }
+    }
+}
---
+++ Tyuiu.HodakovaNA.Sprint1.Task2.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.HodakovaNA.Sprint1.Task2.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.HodakovaNA.Sprint1.Task2.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d68b8028-d09f-40f1-9483-b6bee49d85ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
