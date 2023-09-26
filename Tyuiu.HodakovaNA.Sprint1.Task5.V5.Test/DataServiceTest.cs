a90fea8d91e2a059f841a00ca996033ca3122427
HodakovaNA <144614378+HodakovaNA@users.noreply.github.com>
2023-09-26 17:56:31 +0500

    Task5

---
+++ Tyuiu.HodakovaNA.Sprint1.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.HodakovaNA.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.HodakovaNA.Sprint1.Task5.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x = 32.597;
+            DataService ds = new DataService();
+            var res = ds.Calculate(x);
+            int d = Convert.ToInt32(res);
+            int wait = 5;
+            Assert.AreEqual(wait, d);
+        }
+    }
+}
---
+++ Tyuiu.HodakovaNA.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.HodakovaNA.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.HodakovaNA.Sprint1.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            //Написать программу, которая решает следующую задачу:
+            //Присвоить целой переменной d первую цифру из дробной части положительного
+            //вещественного числа x(так, если x = 32.597, то d = 5).
+
+
+
+            Console.Title = "Спринт #1 | Выполнилf Ходакова Н.А. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                       *");
+            Console.WriteLine("* Тема: Базовые навыки работы с C#                                                                *");
+            Console.WriteLine("* Задание #4                                                                                      *");
+            Console.WriteLine("* Вариант #25                                                                                     *");
+            Console.WriteLine("* Выполнила: Ходакова Надежда Антоновна | АСОиУб-23-1                                             *");
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                        *");
+            Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части положительного                       *");
+            Console.WriteLine("* вещественного числа x (так, если x = 32.597, то d = 5).                                         *");
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ ( Вводить через ','):                                                           *");
+            Console.WriteLine("***************************************************************************************************");
+
+            double x;
+            Console.Write("Введите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                      *");
+            Console.WriteLine("***************************************************************************************************");
+
+            Console.WriteLine("d = " + ds.Calculate(x));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
